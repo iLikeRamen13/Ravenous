071fa02034c255ae7dc7bd57e6724cd73f345be2
Niam Samani <niam@laptoop.broadband>
2026-01-13 20:17:25 +0000

    Cross referenced with example code and all is good (mostly)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
 import './App.css'
-import Business from './components/Business/Business.jsx'
 import BusinessList from './components/BusinessList/BusinessList.jsx'
 import Searchbar from './components/Searchbar/Searchbar.jsx'
 
