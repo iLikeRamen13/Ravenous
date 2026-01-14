c5f43aee7a1cf84380271d37df05432d63565f5e
Niam Samani <niam@laptoop.broadband>
2026-01-14 07:35:31 +0000

    Pressed tab a bunch of times in searchbar.css, it did stuff not it looks sexy. Noice.

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -4,12 +4,15 @@ import './Searchbar.css'
 // Define the searchbar component
 function Searchbar() {
     return (
-        <div className="Searchbar">
+       <div>
+       <div className="Searchbar">
             <input type="text" placeholder="Search Businesses" id='business-search' />
             <input type="text" placeholder="Where?" id='location-search' />
-            <br />
-            <button>Let's Go</button>
         </div>
+        <br />
+        <button>Let's Go</button>
+        </div>
+
     )
 }
 
