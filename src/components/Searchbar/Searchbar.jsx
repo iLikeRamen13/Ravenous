8f314fae092cc9efa0e46fbf780480b51dbe75e3
Niam Samani <niam@laptoop.broadband>
2026-01-16 17:50:05 +0000

    Added filter buttons, updated css and started to add hooks

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -3,15 +3,41 @@ import './Searchbar.css'
 
 // Define the searchbar component
 function Searchbar() {
+    // Initialize a state variable for the search term
+    const [searchTerm, setSearchTerm] = useState('');
+    const [location, setLocation] = useState('');
+    const [sortBy, setSortBy] = useState('best_match');
     return (
-       <div>
-       <div className="Searchbar">
-            <input type="text" placeholder="Search Businesses" id='business-search' />
-            <input type="text" placeholder="Where?" id='location-search' />
+    
+    <div>
+        <div className='filters'>
+        <div className='Best-Match'>
+            <button>Best Match</button>
         </div>
+
+        <div className='Highest-Rated'>
+            <button>Highest Rated</button>
+        </div>
+
+        <div className='Most-Reviewed'>
+            <button>Most Reviewed</button>
+        </div>
+        </div>
+
         <br />
-        <button>Let's Go</button>
+
+        <div className="Searchbar">
+                <input type="text" placeholder="Search Businesses" id='business-search' />
+                <input type="text" placeholder="Where?" id='location-search' />
         </div>
+            
+            <br />
+        
+        <div className='search'>
+            <button>Let's Go</button>
+        </div>
+    
+    </div>
 
     )
 }
